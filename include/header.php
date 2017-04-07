4d4842ca016baef15fc20c308412d13f924e13a8
Skuth <the.skuth@gmail.com>
2017-04-07 00:43:52 -0300

    Css min

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no">
 	<title>Painel Admin</title>
-	<link rel="stylesheet" type="text/css" href="lib/css/style.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/style.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="lib/css/font-awesome.min.css">
 	<script type="text/javascript" src="lib/js/jquery.min.js"></script>
